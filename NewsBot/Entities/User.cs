a29bca25cb3739e181cac6ccbad87ef9cc5b3a05
Ali Taami <alitaami81@gmail.com>
2023-12-22 20:44:16 +0330

    Downloaded EF packages

--- NewsBot/Entities/User.cs
+++ NewsBot/Entities/User.cs
@@ -1,9 +1,12 @@
 ﻿using NewsBot.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NewsBot.Entities
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string? LastName { get; set; }
@@ -11,6 +14,8 @@ namespace NewsBot.Entities
         public long ChatId { get; set; }
         public UserType UserType { get; set; }
         public int? ParentId { get; set; }
+
+        [ForeignKey(nameof(ParentId))]
         public User Parent { get; set; }
     }
 
