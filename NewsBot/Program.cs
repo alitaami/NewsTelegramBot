787daa1d9981c10e4fab7cbd46af1e0d548f511b
Ali Taami <alitaami81@gmail.com>
2023-12-23 21:17:43 +0330

    Added IRepository

--- NewsBot/Program.cs
+++ NewsBot/Program.cs
@@ -1,14 +1,24 @@
+using Data.Repositories;
 using FarzamNews.Utilities;
 using Microsoft.EntityFrameworkCore;
 using NewsBot.Data.NewsContext;
+using NewsBot.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<NewsContext>(options =>
-options.UseSqlServer(connectionString: builder.Configuration.GetConnectionString("DbConnection")));
+    options.UseSqlServer(connectionString: builder.Configuration.GetConnectionString("DbConnection")));
 
+builder.Services.AddScoped<IRepository<News>, Repository<News>>();
+builder.Services.AddScoped<IRepository<User>, Repository<User>>();
+builder.Services.AddScoped<IRepository<NewsKeyWord>, Repository<NewsKeyWord>>();
+builder.Services.AddScoped<IRepository<NewsUserCollection>, Repository<NewsUserCollection>>();
+builder.Services.AddScoped<IRepository<UserActivity>, Repository<UserActivity>>();
+builder.Services.AddScoped<IRepository<KeyWord>, Repository<KeyWord>>();
+ 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +27,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 var context = builder.Services.BuildServiceProvider().GetRequiredService<NewsContext>();
-DataInitializer.Initialize(context,app.Configuration);
+DataInitializer.Initialize(context, app.Configuration);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
