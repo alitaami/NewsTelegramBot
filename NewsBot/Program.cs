5267318d51ab3dc8f346c3809a08ea08fb88f2d2
ali taami <alitaami81@gmail.com>
2024-02-03 23:29:16 +0330

    Added mapping configurations

--- NewsBot/Program.cs
+++ NewsBot/Program.cs
@@ -1,20 +1,29 @@
+using AutoMapper;
 using Data.Repositories;
 using FarzamNews.Utilities;
 using Microsoft.EntityFrameworkCore;
 using NewsBot.Data.NewsContext;
 using NewsBot.Entities;
+using NewsBot.Mappings;
 using NewsBot.Services;
 using NewsBot.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<NewsContext>(options => 
+builder.Services.AddDbContext<NewsContext>(options =>
     options.UseSqlServer(connectionString: builder.Configuration.GetConnectionString("DbConnection")));
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-builder.Services.AddScoped<INewsService,NewsService>();
- 
+builder.Services.AddScoped<INewsService, NewsService>();
+
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new MappingProfile());
+});
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,7 +32,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 var context = builder.Services.BuildServiceProvider().GetRequiredService<NewsContext>();
-DataInitializer.Initialize(context,app.Configuration);
+DataInitializer.Initialize(context, app.Configuration);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
